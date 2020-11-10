ef55e1b60cae9619fc81119de0c8819637e2d5d0
rachelrj <rachel@brainjoltmedia.com>
2020-11-10 16:14:38 -0700

    Remove child photos

--- src/Instruction.js
+++ src/Instruction.js
@@ -27,25 +27,16 @@ class Instruction extends React.Component {
                     </div>
                     <div id="overflow">
                     <div class="instruction" id="right">
-                        <img src={img2}/>
-                        <span id="instruction-photo-note">A photograph of my students at a public school in Colorado</span>
+                        <img src={img4}/>
+                        <span id="instruction-photo-note">Artwork done by a student of mine. The students "relaxed and kicked their shoes off" and drew pictures in graphite</span>
                     </div>
-                    <p class="instruction" id="left">There are many advantages to private art tutoring. Art making has therapeutic benefits and promotes mental health and well being. Private art tutoring also can improve a portfolio for an art school application. Private art tutoring can also be a way to create a sentimental and personalized gift for a loved one.  I  will work with you to address your specific needs and create an individualized plan to pursue you or your child’s goals as an artist. </p>
+                        <p class="instruction" id="left">There are many advantages to private art tutoring. Art making has therapeutic benefits and promotes mental health and well being. Private art tutoring also can improve a portfolio for an art school application. Private art tutoring can also be a way to create a sentimental and personalized gift for a loved one.  I  will work with you to address your specific needs and create an individualized plan to pursue you or your child’s goals as an artist. </p>
                     </div>
                 </div>
-                <h2>Student Highlights</h2>
-                        <span class="student-work">
-                            <img src={img3}/>
-                            <span id="instruction-photo-note">Artwork done by a student of mine. The assignment was to transform pencils into an animal of your choice</span>
-                        </span>
-                        <span class="student-work">
-                            <img src={img4}/>
-                            <span id="instruction-photo-note">Artwork done by a student of mine. The students "relaxed and kicked their shoes off" and drew pictures in graphite</span>
-                        </span>
-                        <span class="student-work">
-                            <img src={img5}/>
-                            <span id="instruction-photo-note">Me with an art student in Bronx, New York</span>
-                        </span>
+                    <div id="instruction-content">
+                        <img src={img3}/>
+                        <span id="instruction-photo-note">Artwork done by a student of mine. The assignment was to transform pencils into an animal of your choice</span>
+                    </div>
                 <Footer/>
             </Provider>
         );
