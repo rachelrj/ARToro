fec6cef0b4c3b626ecb25143b9c187ab75ce9d64
Rachel Joyce <rjoyce@splunk.com>
2024-04-22 21:22:46 -0600

    DELETE the photo of the oil landscape. Move Laredo painting to the top. Switch main private art instruction photo. Add README.

--- src/Instruction.js
+++ src/Instruction.js
@@ -1,63 +1,110 @@
-import React from 'react';
-import Footer from './Footer';
-import Header from './Header';
-import { Provider } from 'react-redux';
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import { Provider } from "react-redux";
 import store from "./redux/store";
-import {connect} from "react-redux";
-import lexiToroImage from './img/alexis-toro-private-art-instruction.jpg';
-import studentSample1 from './img/student-sample-1.jpg';
-import studentSample2 from './img/student-sample-2.jpg';
-import student1 from './img/alexis-toro-art-instruction-students1.jpg';
-import student2 from './img/alexis-toro-art-instruction-students2.jpg';
+import { connect } from "react-redux";
+import lexiToroImage from "./img/Alexis-Toro-Art-Instruction.jpeg";
+import studentSample1 from "./img/student-sample-1.jpg";
+import studentSample2 from "./img/student-sample-2.jpg";
+import student1 from "./img/alexis-toro-art-instruction-students1.jpg";
+import student2 from "./img/alexis-toro-art-instruction-students2.jpg";
 
 class Instruction extends React.Component {
-
-    render() {
-        return (
-            <Provider store = {store}>
-                <Header additionalClass="main-header"/>
-                <h1>Private Art Instruction</h1>
-                <div class="instruction-content">
-                    <div id="overflow">
-                        <span className="left instruction">
-                            <img src={lexiToroImage} alt="Alexis Toro, private art instructor"/>
-                        </span>
-                        <span className="right instruction">
-                            <p>I am a certified Pre-K-12th grade art teacher in the state of Montana and Colorado. I earned my Masters degree in Art Education at Florida State University in 2016. My teaching career spans over seven years in which I have taught elementary, middle, and high school. In 2020 I began privately tutoring adults and children. While my focus is primarily painting, I tutor in any visual arts form of your choice including drawing, oil, acrylic, and watercolor painting, as well as basic book binding.</p>
-                        </span>
-                    </div>
-                    <div id="overflow">
-                    <div className="instruction right">
-                        <img src={studentSample1} alt="Art done by a student of Alexis Toro in graphite"/>
-                        <span id="instruction-photo-note">Artwork done by a student of mine. The assignment was to transform pencils into an animal of your choice</span>
-                    </div>
-                        <p className="instruction left">There are many advantages to private art tutoring. Art making has therapeutic benefits and promotes mental health and well being. Private art tutoring also can improve a portfolio for an art school application. Private art tutoring can also be a way to create a sentimental and personalized gift for a loved one.  I  will work with you to address your specific needs and create an individualized plan to pursue you or your child’s goals as an artist. </p>
-                    </div>
-                </div>
-                <div className="instruction-content">
-                    <img src={studentSample2} alt="Art done by a student of Alexis Toro. The student drew a bird made of pencils."/>
-                    <span id="instruction-photo-note">Artwork done by a student of mine. The students "relaxed and kicked their shoes off" and drew pictures in graphite</span>
-                </div>
-                <div className="instruction-content">
-                    <div id="overflow">
-                        <span className="left instruction">
-                            <img src={student1} alt="Home school private art instruction by Alexis Toro"/>
-                        </span>
-                        <span className="right instruction">
-                            <img src={student2} alt="Home school private art instruction by Alexis Toro"/>
-                        </span>
-                    </div>
-                    <span id="instruction-photo-note">Photos from a home school private art instruction class. The girls are extremely proud of their work!</span>
-                </div>
-                <hr className="dotted"></hr>
-                <p className="instruction-content">Tutoring is done in your home with whatever materials you wish to provide. I will provide a preferred shopping list upon request. I am located in Frenchtown, but I do travel to Missoula and surrounding areas. Please inquire for pricing. </p>
-                <Footer/>
-            </Provider>
-        );
-    }
-}
-const mapStateToProps = state => {
-  return {
+  render() {
+    return (
+      <Provider store={store}>
+        <Header additionalClass='main-header' />
+        <h1>Private Art Instruction</h1>
+        <div class='instruction-content'>
+          <div id='overflow'>
+            <span className='left instruction'>
+              <img
+                id='instructor-photo'
+                src={lexiToroImage}
+                alt='Alexis Toro, private art instructor'
+              />
+            </span>
+            <span className='right instruction'>
+              <p>
+                I am a certified Pre-K-12th grade art teacher in the state of
+                Montana and Colorado. I earned my Masters degree in Art
+                Education at Florida State University in 2016. My teaching
+                career spans over seven years in which I have taught elementary,
+                middle, and high school. In 2020 I began privately tutoring
+                adults and children. While my focus is primarily painting, I
+                tutor in any visual arts form of your choice including drawing,
+                oil, acrylic, and watercolor painting, as well as basic book
+                binding.
+              </p>
+            </span>
+          </div>
+          <div id='overflow'>
+            <div className='instruction right'>
+              <img
+                src={studentSample1}
+                alt='Art done by a student of Alexis Toro in graphite'
+              />
+              <span id='instruction-photo-note'>
+                Artwork done by a student of mine. The assignment was to
+                transform pencils into an animal of your choice
+              </span>
+            </div>
+            <p className='instruction left'>
+              There are many advantages to private art tutoring. Art making has
+              therapeutic benefits and promotes mental health and well being.
+              Private art tutoring also can improve a portfolio for an art
+              school application. Private art tutoring can also be a way to
+              create a sentimental and personalized gift for a loved one. I will
+              work with you to address your specific needs and create an
+              individualized plan to pursue you or your child’s goals as an
+              artist.{" "}
+            </p>
+          </div>
+        </div>
+        <div className='instruction-content'>
+          <img
+            src={studentSample2}
+            alt='Art done by a student of Alexis Toro. The student drew a bird made of pencils.'
+          />
+          <span id='instruction-photo-note'>
+            Artwork done by a student of mine. The students "relaxed and kicked
+            their shoes off" and drew pictures in graphite
+          </span>
+        </div>
+        <div className='instruction-content'>
+          <div id='overflow'>
+            <span className='left instruction'>
+              <img
+                src={student1}
+                alt='Home school private art instruction by Alexis Toro'
+              />
+            </span>
+            <span className='right instruction'>
+              <img
+                src={student2}
+                alt='Home school private art instruction by Alexis Toro'
+              />
+            </span>
+          </div>
+          <span id='instruction-photo-note'>
+            Photos from a home school private art instruction class. The girls
+            are extremely proud of their work!
+          </span>
+        </div>
+        <hr className='dotted'></hr>
+        <p className='instruction-content'>
+          Tutoring is done in your home with whatever materials you wish to
+          provide. I will provide a preferred shopping list upon request. I am
+          located in Frenchtown, but I do travel to Missoula and surrounding
+          areas. Please inquire for pricing.{" "}
+        </p>
+        <Footer />
+      </Provider>
+    );
   }
+}
+const mapStateToProps = (state) => {
+  return {};
 };
 export default connect(mapStateToProps)(Instruction);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import {createStore} from "redux";
-import reducer from './reducer';
-import STORETYPES from './storeTypes';
+import { createStore } from "redux";
+import reducer from "./reducer";
+import STORETYPES from "./storeTypes";
 
 let initialState = {};
 
@@ -9,129 +9,139 @@ let initialState = {};
 // Oil = 2
 // Miscellaneous = 3
 
-initialState[STORETYPES[0]] = [{
-    'id': 1,
-    'title': 'Mocha',
-    'img': '../img/Mocha.png',
-    'size': '8”x10” Oval',
-    'status': 'Sold'
-},{
-    'id': 2,
-    'title': 'Landscape',
-    'img': '../img/oil-painting-1.png',
-    'size': '18x24 Inches',
-    'status': 'Sold'
-},{
-    'id': 3,
-    'title': 'Cinnamon',
-    'img': '../img/cinnamon.jpg',
-    'size': '18x24 Inches',
-    'status': 'Sold'
-},{
-    'id': 4,
-    'title': 'Leather Painting',
-    'img': '../img/leather-painting.jpg',
-    'size': '',
-    'status': 'Sold'
-}, {
-    'id': 5,
-    'title': "Long's peak from Longmont",
-    'img': '../img/watercolor-painting-2.png',
-    'size': '',
-    'status': 'Sold'
-}, {
-    'id': 6,
-    'title': "Juan Deer",
-    'img': '../img/juan-deer.jpg',
-    'size': '24x24 Inches',
-    'status': 'Sold'
-},{
-    'id': 7,
-    'title': "Tally and Landis",
-    'img': '../img/tally_landis.jpg',
-    'size': '12x16 Inches',
-    'status': 'Sold'
-}];
-initialState[STORETYPES[1]] = [{
-    'id': 2,
-    'title': 'Brainard Lake',
-    'img': '../img/watercolor-donation.jpg',
-    'size': 'Donation to Plenty of',
-    'status': 'Pitbulls Auction'
-},{
-    'id': 3,
-    'title': 'Elk Mountain',
-    'img': '../img/watercolor-1.jpg',
-    'size': '8x10',
-    'status': 'Sold'
-},{
-    'id': 3,
-    'title': 'Waterlogged',
-    'img': '../img/waterlogged.jpg',
-    'size': '6x12',
-    'status': 'For Sale'
-},{
-    'id': 1,
-    'title': "Long's peak from Longmont",
-    'img': '../img/watercolor-painting-2.jpg',
-    'size': '',
-    'status': 'Sold'
-},{
-    'id': 3,
-    'title': 'Marvine Lakes',
-    'img': '../img/watercolor-painting-4.jpg',
-    'size': '6x12',
-    'status': 'For Sale'
-},];
-initialState[STORETYPES[2]] = [{
-    'id': 2,
-    'title': 'Landscape',
-    'img': '../img/oil-painting-1.jpg',
-    'size': '18x24',
-    'status': 'Sold'
-},{
-    'id': 1,
-    'title': 'Mocha',
-    'img': '../img/Mocha.png',
-    'size': '8”x10” Oval',
-    'status': 'Sold'
-},{
-    'id': 3,
-    'title': 'Cinnamon',
-    'img': '../img/cinnamon.jpg',
-    'size': '18x24 Inches',
-    'status': 'Sold'
-}, {
-    'id': 4,
-    'title': "Juan Deer",
-    'img': '../img/juan-deer.jpg',
-    'size': '24x24 Inches',
-    'status': 'Sold'
-},{
-    'id': 5,
-    'title': "Tally and Landis",
-    'img': '../img/tally_landis.jpg',
-    'size': '12x16 Inches',
-    'status': 'Sold'
-},{
-    'id': 6,
-    'title': "Blake",
-    'img': '../img/Blake.png',
-    'size': '8”x10” Oval',
-    'status': 'Sold'
-},{
-    'id': 7,
-    'title': "Rib Breaker",
-    'img': '../img/Laredo.jpg',
-    'size': '18x24 Inches',
-    'status': 'For Sale'
-}];
-initialState[STORETYPES[3]] = [{
-    'id': 1,
-    'title': 'Leather Painting',
-    'img': '../img/leather-painting.jpg',
-    'size': '',
-    'status': 'Sold'
-}];
+initialState[STORETYPES[0]] = [
+  {
+    id: 1,
+    title: "Mocha",
+    img: "../img/Mocha.png",
+    size: "8”x10” Oval",
+    status: "Sold",
+  },
+  {
+    id: 3,
+    title: "Cinnamon",
+    img: "../img/cinnamon.jpg",
+    size: "18x24 Inches",
+    status: "Sold",
+  },
+  {
+    id: 4,
+    title: "Leather Painting",
+    img: "../img/leather-painting.jpg",
+    size: "",
+    status: "Sold",
+  },
+  {
+    id: 5,
+    title: "Long's peak from Longmont",
+    img: "../img/watercolor-painting-2.png",
+    size: "",
+    status: "Sold",
+  },
+  {
+    id: 6,
+    title: "Juan Deer",
+    img: "../img/juan-deer.jpg",
+    size: "24x24 Inches",
+    status: "Sold",
+  },
+  {
+    id: 7,
+    title: "Tally and Landis",
+    img: "../img/tally_landis.jpg",
+    size: "12x16 Inches",
+    status: "Sold",
+  },
+];
+initialState[STORETYPES[1]] = [
+  {
+    id: 2,
+    title: "Brainard Lake",
+    img: "../img/watercolor-donation.jpg",
+    size: "Donation to Plenty of",
+    status: "Pitbulls Auction",
+  },
+  {
+    id: 3,
+    title: "Elk Mountain",
+    img: "../img/watercolor-1.jpg",
+    size: "8x10",
+    status: "Sold",
+  },
+  {
+    id: 3,
+    title: "Waterlogged",
+    img: "../img/waterlogged.jpg",
+    size: "6x12",
+    status: "For Sale",
+  },
+  {
+    id: 1,
+    title: "Long's peak from Longmont",
+    img: "../img/watercolor-painting-2.jpg",
+    size: "",
+    status: "Sold",
+  },
+  {
+    id: 3,
+    title: "Marvine Lakes",
+    img: "../img/watercolor-painting-4.jpg",
+    size: "6x12",
+    status: "For Sale",
+  },
+];
+initialState[STORETYPES[2]] = [
+  {
+    id: 7,
+    title: "Rib Breaker",
+    img: "../img/Laredo.jpg",
+    size: "18x24 Inches",
+    status: "For Sale",
+  },
+  {
+    id: 1,
+    title: "Mocha",
+    img: "../img/Mocha.png",
+    size: "8”x10” Oval",
+    status: "Sold",
+  },
+  {
+    id: 3,
+    title: "Cinnamon",
+    img: "../img/cinnamon.jpg",
+    size: "18x24 Inches",
+    status: "Sold",
+  },
+  {
+    id: 4,
+    title: "Juan Deer",
+    img: "../img/juan-deer.jpg",
+    size: "24x24 Inches",
+    status: "Sold",
+  },
+  {
+    id: 5,
+    title: "Tally and Landis",
+    img: "../img/tally_landis.jpg",
+    size: "12x16 Inches",
+    status: "Sold",
+  },
+  {
+    id: 6,
+    title: "Blake",
+    img: "../img/Blake.png",
+    size: "8”x10” Oval",
+    status: "Sold",
+  },
+];
+initialState[STORETYPES[3]] = [
+  {
+    id: 1,
+    title: "Leather Painting",
+    img: "../img/leather-painting.jpg",
+    size: "",
+    status: "Sold",
+  },
+];
 
 export default createStore(reducer, initialState);
